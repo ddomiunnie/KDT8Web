b9b24485bf4ef856fb6590642ddf562eee16db79
ddomiunnie <sjookim417@naver.com>
2023-08-10 13:47:03 +0900

    feat: JOIN study

---
+++ 15_sequelize_example/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+const db = require('./models');
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+const router = require('./routes/student');
+app.use('/', router);
+
+db.sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+  });
+});
