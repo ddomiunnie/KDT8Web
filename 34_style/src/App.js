6852d853da40135310c346e49e860108c001c6fe
ddomiunnie <sjookim417@naver.com>
2023-09-25 12:32:45 +0900

    feat: sass 실습

---
+++ 34_style/src/App.js
@@ -0,0 +1,17 @@
+// import SassComponent from './SassComponent';
+// import Style from './Style';
+//import Caterpillar from './Caterpillar';
+import StylePrac2 from './StylePrac2';
+
+function App() {
+  return (
+    <>
+      {/* <Style />
+      <SassComponent /> */}
+      {/* <Caterpillar /> */}
+      <StylePrac2 />
+    </>
+  );
+}
+
+export default App;
---
+++ 34_style/src/StylePrac1.js
@@ -0,0 +1,15 @@
+export default function StylePrac1() {
+  return (
+    <div className="lavar">
+      <div className="body body1">
+        <div className="eye_black"></div>
+        <div className="eye_white"></div>
+      </div>
+      <div className="body_1"></div>
+      <div className="body_2"></div>
+      <div className="body_3"></div>
+      <div className="body_4"></div>
+      <div className="body_5"></div>
+    </div>
+  );
+}
