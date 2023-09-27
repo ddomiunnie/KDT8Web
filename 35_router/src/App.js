4838dda6f2aa78937195f00ad0bc23820fb453a2
ddomiunnie <sjookim417@naver.com>
2023-09-27 10:26:24 +0900

    feat: 라우터 실습

--- 35_router/src/App.js
+++ 35_router/src/App.js
@@ -1,15 +1,17 @@
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
+// import { Outlet } from 'react-router-dom';
+// import Header from './Header';
 //import Router from './Router';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       {/* ver2 */}
-      <Header />
-      <Outlet />
+      {/* <Header />
+      <Outlet /> */}
       {/* ver1 */}
       {/* <Router /> */}
+      <Main />
     </div>
   );
 }
